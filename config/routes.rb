7291b91191e071859c3a2a9fe71ea1142aef4014
Sheri Wilson <smgwilson37@gmail.com>
2016-02-21 13:11:47 -0800

    Create new and edit forms and partials for recipes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,15 @@ Rails.application.routes.draw do
 
   root 'pages#home'
   get '/home', to: 'pages#home'
+  # get '/recipes', to:'recipes#index'
+  # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+  # post '/recipes', to: 'recipes#create'
+  # get '/recipes/:id/edit', to:'recipes#edit', as: 'edit_recipe'
+  # patch '/recipes/:id', to: 'recipes#update'
+  # get '/recipes/:id/show', to: 'recipes#show', as: 'recipe'
+  # delete '/recipes/:id', to: 'recipes#destroy'
 
+  resources :recipes
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
